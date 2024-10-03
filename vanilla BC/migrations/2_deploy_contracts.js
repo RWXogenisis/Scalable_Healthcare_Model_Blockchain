050f559854f7a9e00476a5d5cac2d6bd8d020c8b
S Karun Vikhash <65536776+RWXogenisis@users.noreply.github.com>
2024-10-03 13:53:41 +0530

    Delete vanilla BC directory

--- vanilla BC/migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const DoctorContract = artifacts.require("DoctorContract");
-const PatientContract = artifacts.require("PatientContract");
-const AppointmentContract = artifacts.require("AppointmentContract");
-
-module.exports = function (deployer) {
-  deployer.deploy(DoctorContract);
-  deployer.deploy(PatientContract);
-  deployer.deploy(AppointmentContract);
-};
